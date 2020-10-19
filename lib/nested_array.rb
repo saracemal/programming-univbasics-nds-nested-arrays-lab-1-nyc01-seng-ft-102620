e0f7516788081a3b4262b600e2b6bbf0c11bdcd2
Sara Cemal <saracemal@gmail.com>
2020-10-19 00:40:14 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,8 +32,11 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  nested_array = [
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+    ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
+    ]
+nested_array[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
